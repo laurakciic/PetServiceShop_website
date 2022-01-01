ce0752eae60f2cd9d83ddd7c40d887106367dcb8
Laura Kovačić <laurakciic@gmail.com>
2022-01-01 16:13:29 +0100

    separate and edit shop section

--- shop.php
+++ shop.php
@@ -15,7 +15,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <!-- custom css file link -->
-    <link rel="stylesheet" href="css/main/style.css">
+    <link rel="stylesheet" href="css/shop/shop.css">
+
+    <!-- bootstrap cdn link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 </head>
 <body>
@@ -26,10 +29,7 @@
         <a href="#" class="logo"> <i class="fas fa-paw"></i> shop </div></a>
 
         <nav class="navbar">
-            <a href="#home">home</a> 
-            <a href="#about">about</a> 
-            <a href="shop.php">shop</a> 
-            <a href="#share">social</a>
+            <a href="main.php">home</a> 
         </nav>
 
         <div class="icons">
@@ -58,66 +58,15 @@
     <!-- home section start --> 
     <section class="home" id="home">
 
-        <div class="content">
-            <h3><span>hi</span> welcome to our pet shop</h3>
-            <a href="shop.php" class="btn">shop now</a>
-        </div>
-
         <img src="images/bottom_wave.png" class="wave" alt="">
 
     </section>
     <!-- home section end --> 
 
-    <!-- about section start --> 
-    <section class="about" id="about">
-
-        <div class="image">
-            <img src="images/about.png" alt="">
-        </div>
-
-        <div class="content">
-            <h3>premium <span>pet food</span> manufacturer</h3>
-            <p>The website you are currently seeing represents a project made for Web Programming course that I study at FERIT in Osijek.</p>
-            <a href="#dog-food" class="btn">read more</a>
-        </div>
-
-    </section>
-    <!-- about section end --> 
-
-    <!-- dog and cat food banner start --> 
-    <div class="dog-food" id="dog-food">
-
-        <div class="image">
-            <img src="images/dog_food.png" alt="">
-        </div>
-
-        <div class="content">
-            <h3> air dried <span> dog food </span> </h3>
-            <p> Premium air dried dog food because we love our dogs more than other humans and want them to live as long and healthy as possible. </p>
-            <a href="#shop"> <img src="images/dog_shopping.JPG" alt=""></a>
-        </div>
-
-    </div>
-
-    <div class="cat-food">
-
-        <div class="content">
-            <h3> air dried <span> cat food </span> </h3>
-            <p> Premium air dried cat food because we love our cat more than other humans and want them to live as long and healthy as possible. </p>
-            <a href="#shop"> <img src="images/cat_shopping.JPG" alt=""></a>
-        </div>
-
-        <div class="image">
-            <img src="images/cat_food.png" alt="">
-        </div>
-
-    </div>
-    <!-- dog and cat food banner end--> 
-
     <!-- shop section start-->
-    <!-- <section class="shop" id="shop">
+    <section class="shop" id="shop">
 
-        <h1 class="heading"> our <span> products </span></h1>
+        <h1 class="heading"> popular <span> products </span></h1>
 
         <div class="box-container">
 
@@ -200,26 +149,22 @@
             </div>
 
         </div>
-    </section> -->
+    </section>
     <!-- shop section end -->
 
     <!-- footer section start -->
     <section class="footer">
         <img src="images/top_wave.png" alt="">
-
-        <div class="share" id="share">
-            <a href="https://hr-hr.facebook.com/laurakovaciic" target="blank" class="btn"> <i class="fab fa-facebook-f"></i> facebook </a>
-            <a href="https://www.instagram.com/laurakciic/" target="blank" class="btn"> <i class="fab fa-instagram"></i> instagram </a>
-            <a href="https://hr.linkedin.com/in/laura-kova%C4%8Di%C4%87-1a973221b?trk=public_profile_samename-profile" target="blank" class="btn"> <i class="fab fa-linkedin"></i> linkedin </a>
-        </div>
-
-        <div class="credit"> created by <span> laura kovačić </span> </div>
-
     </section>
     <!-- footer section end -->
 
     <!-- js file link -->
     <script src="js/script.js"></script>
+
+    <!-- js links -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
 </html>
