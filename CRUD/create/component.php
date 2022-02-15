edcb1aee0a3a01cd334b09d2f8cebf5539bd4568
Laura Kovačić <laurakciic@gmail.com>
2022-02-16 00:06:48 +0100

    add save for later button without implementation

--- CRUD/create/component.php
+++ CRUD/create/component.php
@@ -39,6 +39,7 @@
                 <div class=\"col-md-6\">
                     <h5 class=\"pt-2\">$product_name</h5>
                     <h5 class=\"pt-2\">$product_price kn</h5>
+                    <button type=\"submit\" class=\"btn btn-warning\">Save for Later</button>
                     <button type=\"submit\" class=\"btn btn-danger\" name=\"remove\">Remove</button>
                 </div>
 
